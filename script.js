98c43d892645c2e2bbfe3abb636ea7383c44f305
Hemang Choudhary <hemangc37@gmail.com>
2024-07-21 21:14:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ setTimeout(() => {
     lade.style.display = "none"
     document.getElementById('main').style.display = "block";
 }, 7000);
+
+window.addEventListener('scroll', () => {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    const heroElement = document.getElementById('hero');
+    const height = heroElement.getBoundingClientRect().height;
+    console.log(height);
+    console.log(scrollTop);
+    if (scrollTop > 3*height){
+        document.getElementById('main_anim').style.display = "none";
+    }
+    else if(scrollTop < 3*height){
+        document.getElementById('main_anim').style.display = "block";
+    }
+});
