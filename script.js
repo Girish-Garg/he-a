ac7fdeefde31ddcc25e73878b1c54fc855d79d61
Hemang Choudhary <hemangc37@gmail.com>
2024-07-22 12:48:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -10,10 +10,25 @@ window.addEventListener('scroll', () => {
     const height = heroElement.getBoundingClientRect().height;
     console.log(height);
     console.log(scrollTop);
-    if (scrollTop > 3*height){
+    if (scrollTop > 3*height && scrollTop < 5*height){
         document.getElementById('main_anim').style.display = "none";
+        document.getElementById('big_img').style.display = "block";
+    }
+    else if(scrollTop > 4*height){
+        document.getElementById('main_anim').style.display = "none";
+        document.getElementById('big_img').style.display = "none";
     }
     else if(scrollTop < 3*height){
         document.getElementById('main_anim').style.display = "block";
     }
 });
+
+Shery.mouseFollower({
+  });
+  Shery.makeMagnet("#e1oi2mnWSwv1" /* Element to target.*/, {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
+
+
