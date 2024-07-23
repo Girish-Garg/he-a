6cc11e2c3c69a72a39c5a01e2ab18a0da40b4de6
Girish-Garg <141021178+Girish-Garg@users.noreply.github.com>
2024-07-23 17:49:00 +0530

    more changes done

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const lade = document.getElementById('loader');
-setTimeout(() => {
-    lade.style.display = "none"
-    document.getElementById('main').style.display = "block";
-}, 7000);
+// setTimeout(() => {
+//     lade.style.display = "none"
+//     document.getElementById('main').style.display = "block";
+// }, 7000);
 
 window.addEventListener('scroll', () => {
     const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
@@ -12,15 +12,15 @@ window.addEventListener('scroll', () => {
     const height2 = document.getElementById('Image_pin').getBoundingClientRect().height;
     console.log(scrollTop);
     if (scrollTop > 3 * height && scrollTop < (3 * height + height2 - 20)) {
-        document.getElementById('main_anim').style.display = "none";
+        // document.getElementById('main_anim').style.display = "none";
         document.getElementById('big_img').style.display = "block";
     }
     else if (scrollTop > (3 * height + height2 - 20)) {
-        document.getElementById('main_anim').style.display = "none";
+        // document.getElementById('main_anim').style.display = "none";
         document.getElementById('big_img').style.display = "none";
     }
     else if (scrollTop < 3 * height) {
-        document.getElementById('main_anim').style.display = "block";
+        // document.getElementById('main_anim').style.display = "block";
     }
 });
 
@@ -74,4 +74,12 @@ document.getElementById('img_half_1').addEventListener('mousemove', (e) => {
 document.getElementById('img_half_1').addEventListener('mouseleave', (e) => {
     mouseFollower.style.opacity = "0";
 })
-
+// Shery.imageMasker(".img", {
+    
+//     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+//     duration: 1,
+// });
+Shery.makeMagnet(".img", {
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+});
